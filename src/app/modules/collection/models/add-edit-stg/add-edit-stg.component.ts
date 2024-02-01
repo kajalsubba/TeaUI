73c5e8b798ec0050bf6e6bf40e7e10d8309c5f68
DESKTOP-GEILO6O\Kajal <subba.kajal@hotmail.com>
2024-02-01 14:12:55 +0530

    STG Update

--- src/app/modules/collection/models/add-edit-stg/add-edit-stg.component.ts
+++ src/app/modules/collection/models/add-edit-stg/add-edit-stg.component.ts
@@ -7,7 +7,8 @@ import { AutoCompleteService } from '../../services/auto-complete.service';
 import { IGetFactory } from 'src/app/modules/masters/interfaces/IFactory';
 import { GradeService } from 'src/app/modules/masters/services/grade.service';
 import { IGetGrade } from 'src/app/modules/masters/interfaces/IGrade';
-import { Subscription } from 'rxjs';
+import { Subject, Subscription, takeUntil } from 'rxjs';
+
 
 @Component({
   selector: 'app-add-edit-stg',
@@ -17,11 +18,12 @@ import { Subscription } from 'rxjs';
 export class AddEditStgComponent implements OnInit {
 
   stgForm!:FormGroup;
+  private destroy$ = new Subject<void>();
   currentDate:Date = new Date();
-  vehicleNumbers: string[] = [];
+  vehicleNumbers: any;
   loginDetails: any;
-  clientNames: string[] =[];
-  gradeList:any[]=[];
+  ClientNames: any;
+  GradeList:any[]=[];
   private subscriptions: Subscription[] = [];
 
   constructor(
@@ -34,75 +36,75 @@ export class AddEditStgComponent implements OnInit {
     private gradeService:GradeService
   ){}
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.loginDetails = this.helper.getItem('loginDetails');
       this.stgForm = this.fb.group({
-        collectionDate:[new Date()],
-        vehicleNo:['', Validators.required],
-        clientName:['', Validators.required],
-        firstWeight:[0, Validators.required],
-        wetLeaf:[0, Validators.required],
-        longLeaf:[0, Validators.required],
-        deduction:[0],
-        finalWeight:[0],
-        grade:['', Validators.required],
-        rate:['', Validators.required],
-        remarks:['', Validators.required],
+        CollectionDate:[new Date()],
+        VehicleNo:['', Validators.required],
+        ClientName:['', Validators.required],
+        FirstWeight:[0, Validators.required],
+        WetLeaf:[0, Validators.required],
+        LongLeaf:[0, Validators.required],
+        Deduction:[0],
+        FinalWeight:[0],
+        Grade:['', Validators.required],
+        Rate:['', Validators.required],
+        Remarks:['', Validators.required],
       });
-      this.loadClientNames();
-      this.loadVehicleNumbers();
+     await this.loadClientNames();
+     await this.loadVehicleNumbers();
       this.getFactoryDate();
       this.getGradeList();
   }
 
-  firstWeightInput(value: any) {
+  FirstWeightInput(value: any) {
     console.log(value);
-    this.calculateFinalWeight();
+    this.CalculateFinalWeight();
   }
   
-  longLeafInput(value: any) {
+  LongLeafInput(value: any) {
     this.calculateDeductionAndFinalWeight();
   }
   
-  wetLeafInput(value: any) {
+  WetLeafInput(value: any) {
     this.calculateDeductionAndFinalWeight();
   }
   
-  calculateFinalWeight() {
+  CalculateFinalWeight() {
     const firstWeight = this.stgForm.value.firstWeight || 0;
-    const wetLeafPercentage = (this.stgForm.value.wetLeaf || 0) / 100;
-    const longLeafPercentage = (this.stgForm.value.longLeaf || 0) / 100;
+    const WetLeafPercentage = (this.stgForm.value.WetLeaf || 0) / 100;
+    const LongLeafPercentage = (this.stgForm.value.LongLeaf || 0) / 100;
     
-    // Calculate wetLeaf and longLeaf in kg
-    const wetLeaf = firstWeight * wetLeafPercentage;
-    const longLeaf = firstWeight * longLeafPercentage;
+    // Calculate WetLeaf and LongLeaf in kg
+    const WetLeaf = firstWeight * WetLeafPercentage;
+    const LongLeaf = firstWeight * LongLeafPercentage;
     
-    // Deduction is the sum of wetLeaf and longLeaf
-    const deduction = Math.round(wetLeaf + longLeaf);
+    // Deduction is the sum of WetLeaf and LongLeaf
+    const Deduction = Math.round(WetLeaf + LongLeaf);
     
     // Calculate final weight in kg
-    const finalWeight = Math.round(firstWeight - deduction);
+    const finalWeight = Math.round(firstWeight - Deduction);
     
     // Update the form control
-    this.stgForm.controls['deduction'].setValue(deduction);
+    this.stgForm.controls['Deduction'].setValue(Deduction);
     this.stgForm.controls['finalWeight'].setValue(finalWeight);
   }
   
   calculateDeductionAndFinalWeight() {
     const firstWeight = this.stgForm.value.firstWeight || 0;
-    const wetLeafPercentage = (this.stgForm.value.wetLeaf || 0) / 100;
-    const longLeafPercentage = (this.stgForm.value.longLeaf || 0) / 100;
+    const WetLeafPercentage = (this.stgForm.value.WetLeaf || 0) / 100;
+    const LongLeafPercentage = (this.stgForm.value.LongLeaf || 0) / 100;
   
-    // Calculate wetLeaf and longLeaf in kg
-    const wetLeaf = firstWeight * wetLeafPercentage;
-    const longLeaf = firstWeight * longLeafPercentage;
+    // Calculate WetLeaf and LongLeaf in kg
+    const WetLeaf = firstWeight * WetLeafPercentage;
+    const LongLeaf = firstWeight * LongLeafPercentage;
     
-    // Deduction is the sum of wetLeaf and longLeaf
-    const deduction = Math.round(wetLeaf + longLeaf);
+    // Deduction is the sum of WetLeaf and LongLeaf
+    const Deduction = Math.round(WetLeaf + LongLeaf);
     
     // Update the form controls
-    this.stgForm.controls['deduction'].setValue(deduction);
-    this.calculateFinalWeight();
+    this.stgForm.controls['Deduction'].setValue(Deduction);
+    this.CalculateFinalWeight();
   }
   
   
@@ -118,28 +120,61 @@ export class AddEditStgComponent implements OnInit {
     }
   }
 
-  private loadVehicleNumbers(): void {
-    this.autocompleteService.getVehicleNumbers().subscribe(numbers => {
-      this.vehicleNumbers = numbers;
-    });
-  }
+  async loadVehicleNumbers() {
+    try {
+        const bodyData: IGetGrade = {
+            TenantId: this.loginDetails.TenantId
+        };
 
-  private loadClientNames(): void {
-    this.autocompleteService.getClientNames().subscribe(client => {
-      this.clientNames = client;
-    });
-  }
+        const res: any = await this.autocompleteService.GetVehicleNumbers(bodyData)
+            .pipe(takeUntil(this.destroy$))
+            .toPromise();
+
+        this.vehicleNumbers = res.VehicleDetails;
+
+
+    } catch (error) {
+        console.error('Error:', error);
+        this.toastr.error('Something went wrong.', 'ERROR');
+    }
+}
+
+  // private loadClientNames(): void {
+  //   this.autocompleteService.getClientNames().subscribe(client => {
+  //     this.ClientNames = client;
+  //   });
+  // }
+
+  async loadClientNames() {
+    try {
+        const bodyData: IGetGrade = {
+            TenantId: this.loginDetails.TenantId
+        };
+
+        const res: any = await this.autocompleteService.GetClientNames(bodyData)
+            .pipe(takeUntil(this.destroy$))
+            .toPromise();
+
+        this.ClientNames = res.ClientDetails;
+
+
+    } catch (error) {
+        console.error('Error:', error);
+        this.toastr.error('Something went wrong.', 'ERROR');
+    }
+}
 
   // Autocomplete function
-  filterVehicleNumbers(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.vehicleNumbers.filter(number => number.toLowerCase().includes(filterValue));
-  }
+  filterVehicleNumbers(value: string): any {
+   const filterValue = value.toLowerCase();
+   return this.vehicleNumbers.filter((x:any) => x.VehicleNo.toLowerCase().includes(filterValue));
+ }
 
   // Autocomplete function
   filterClientNames(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.clientNames.filter(client => client.toLowerCase().includes(filterValue));
+   // console.log(this.ClientNames.filter((number:any) => number.toLowerCase().includes(filterValue)),'Clinet');
+    return this.ClientNames.filter((x:any) => x.ClientName.toLowerCase().includes(filterValue));
   }
 
   displayWithFn(value: string): string {
@@ -152,7 +187,7 @@ export class AddEditStgComponent implements OnInit {
     }
 
     const gradeGetService = this.gradeService.GetGrade(bodyData).subscribe((res:any)=>{
-      this.gradeList = res.GradeDetails
+      this.GradeList = res.GradeDetails
     });
 
     this.subscriptions.push(gradeGetService);
--- src/app/modules/collection/services/auto-complete.service.ts
+++ src/app/modules/collection/services/auto-complete.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { IGetGrade } from '../../masters/interfaces/IGrade';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutoCompleteService {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
-  getVehicleNumbers(): Observable<string[]> {
-    // Replace this with your actual data fetching logic
-    const vehicleNumbers: string[] = ['AS019124', 'AS015678', 'AS011234'];
-    return of(vehicleNumbers);
+  GetVehicleNumbers(clientBody:IGetGrade): Observable<string[]> {
+    return this.apiService.post('Master/GetVehicle', clientBody);
   }
 
-  getClientNames(): Observable<string[]> {
-    // Replace this with your actual data fetching logic
-    const clientName: string[] = ['Kajal Subba', 'Akshar Patel', 'Hari Prasad'];
-    return of(clientName);
+  // getClientNames(): Observable<string[]> {
+  //   // Replace this with your actual data fetching logic
+  //   const clientName: string[] = ['Kajal Subba', 'Akshar Patel', 'Hari Prasad'];
+  //   return of(clientName);
+  // }
+
+  GetClientNames(clientBody:IGetGrade): Observable<string[]> {
+    return this.apiService.post('Master/GetClientList', clientBody);
   }
+
+  // getClient(clientBody:IGetClient): Observable<any> {
+  //   return this.apiService.post('Master/GetClient', clientBody);
+  // }
+
 
 }
