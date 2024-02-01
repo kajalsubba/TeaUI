070f01a54aba8b56dee4e6580e49b87cae5cca57
DESKTOP-GEILO6O\Kajal <subba.kajal@hotmail.com>
2024-02-01 20:21:58 +0530

    New API gateway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://72.167.37.70:81',
+    apiUrl: 'http://72.167.37.70:82',
    // apiUrl: 'https://localhost:44325',
   };
   
