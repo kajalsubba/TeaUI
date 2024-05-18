df6ec6c7ca194213a3cc884579fd11af7986b2ad
AkshatShravan <kumarakshat.sln@gmail.com>
2024-05-18 10:50:44 +0530

    Row and Column wise Calculation

--- src/app/modules/reports/components/grade-report/grade-report.component.ts
+++ src/app/modules/reports/components/grade-report/grade-report.component.ts
@@ -21,16 +21,12 @@ export class GradeReportComponent implements OnInit {
   currentDate: Date | null = new Date();
   gradeReportForm!: FormGroup;
   dataSource = new _MatTableDataSource<any>();
-  columns: { columnDef: string; header: string }[] = [
-    { columnDef: 'ClientName', header: 'Client Name' },
-
-  ]
   displayedColumns: string[] = [];
-
+  dynamicColumns: string[] = []; // Dynamic columns for calculation
+  selectedRowIndex: number = -1;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  selectedRowIndex: number = -1;
 
   constructor(
     private fb: FormBuilder,
@@ -45,27 +41,17 @@ export class GradeReportComponent implements OnInit {
       fromDate: [new Date(), Validators.required],
       toDate: [new Date(), Validators.required]
     });
-    // this.dataSource.data = [
-    //   {
-    //     ClientName: 'ABC',
-    //     A: '',
-    //     B: '',
-    //     C: '',
-    //     D: '',
-    //   }
-    // ];
-    // this.displayedColumns = Object.keys(this.dataSource.data[0]);
 
+    // Fetch data on component load
+    this.search();
   }
 
   ngAfterViewInit() {
-
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
   fromDateChange(event: MatDatepickerInputEvent<Date>): void {
-   // this.gradeReportForm.controls['toDate'].setValue(null);
     this.minToDate = event.value;
   }
 
@@ -79,13 +65,13 @@ export class GradeReportComponent implements OnInit {
         FromDate: formatDate(this.gradeReportForm.value.fromDate, 'yyyy-MM-dd', 'en-US'),
         ToDate: formatDate(this.gradeReportForm.value.toDate, 'yyyy-MM-dd', 'en-US'),
         TenantId: this.loginDetails.TenantId
-
       };
       const res: any = await this.reportService.GetClientWiseGradeReport(bodyData).toPromise();
       const { GradeReport } = res;
       if (GradeReport.length > 0) {
         this.dataSource.data = GradeReport;
-        this.displayedColumns = Object.keys(this.dataSource.data[0]);
+        this.displayedColumns = Object.keys(this.dataSource.data[0]).concat('Total');
+        this.dynamicColumns = this.displayedColumns.filter(col => col !== 'ClientName' && col !== 'Total');
         this.dataSource.data.forEach(item => {
           for (const key in item) {
             if (item.hasOwnProperty(key) && item[key] === null) {
@@ -94,16 +80,26 @@ export class GradeReportComponent implements OnInit {
           }
         });
       }
-
     } catch (error) {
       console.error('Error:', error);
       this.toastr.error('Something went wrong.', 'ERROR');
     }
   }
 
+  getRowTotal(row: any) {
+    return this.dynamicColumns.map(col => row[col]).reduce((acc, value) => acc + value, 0);
+  }
+
+  getColumnTotal(column: string) {
+    return this.dataSource.data.map(t => t[column]).reduce((acc, value) => acc + value, 0);
+  }
+
+  getGrandTotal() {
+    return this.dynamicColumns.map(col => this.getColumnTotal(col)).reduce((acc, value) => acc + value, 0);
+  }
 
   selectRow(row: any, index: number) {
-    this.selectedRowIndex = index; // Set the selected row index
+    this.selectedRowIndex = index;
   }
 
   @HostListener('document:keydown', ['$event'])
@@ -118,5 +114,4 @@ export class GradeReportComponent implements OnInit {
       }
     }
   }
-
 }
